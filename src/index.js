129ecb3797467d517c8199e586abe1fb48a5faf2
Jeff Hanke <geophree@gmail.com>
2020-07-22 22:35:47 -0700

    Switch to Snowpack.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
-import { html } from "htm/react";
-import ReactDOM from "react-dom";
-import { RecoilRoot } from "recoil";
-import App from "./App.js";
+import { html } from 'htm/react';
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom';
+import { RecoilRoot } from 'recoil';
+import App from './App.js';
 
 ReactDOM.render(
-  html`<${RecoilRoot}><${App} /><//>`,
-  document.getElementById("root")
+  html`<${StrictMode}
+    ><${RecoilRoot}><${App} /><//
+  ><//>`,
+  document.getElementById('root'),
 );
+
+// Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
+// Learn more: https://www.snowpack.dev/#hot-module-replacement
+if (import.meta.hot) {
+  import.meta.hot.accept();
+}
