60bbe9672ba3dce64944f239bb658c2bdc4f08e4
Jeff Hanke <geophree@gmail.com>
2020-08-16 20:55:32 -0700

    Fix build for subdir, upgrade snowpack to 2.7 .

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,13 +1,20 @@
-const scripts = {
-  "mount:public": "mount public --to /",
-  "mount:src": "mount src --to /_dist_",
-};
-
 module.exports = {
-  scripts,
-  plugins: ["@snowpack/plugin-babel", "@snowpack/plugin-dotenv"],
-  devOptions: {},
+  mount: {
+    public: "/gameshow",
+    src: "/gameshow/dist",
+    web_modules: "/gameshow/dist/web_modules",
+  },
+  plugins: ["@snowpack/plugin-dotenv"],
+  devOptions: {
+    fallback: "/gameshow/404.html",
+  },
   installOptions: {
     namedExports: ["recoil"],
+  },
+  buildOptions: {
+    metaDir: "/gameshow/dist/snowpack",
+    webModulesUrl: "/gameshow/dist/web_modules",
+    minify: false,
+    clean: true,
   },
 };
