2e34f1d27728551eeb2a4d5ef1ad189882b5a694
Jeff Hanke <geophree@gmail.com>
2020-12-15 02:54:52 -0800

    Remove last bits from prototype, daily double works.
    Still need to stop clicking while fullscreening or flipping.

--- src/jeopardy/index.js
+++ src/jeopardy/index.js
@@ -1,71 +1,10 @@
 import { html } from "htm/react";
-import { atom, atomFamily, useRecoilValue } from "recoil";
+import { atom, atomFamily, useRecoilState, useRecoilValue } from "recoil";
 
 import "./style.css";
 import gyparodyFontUrl from "./fonts/gyparody.woff2";
 import OptiKorinnaFontUrl from "./fonts/OPTIKorinna-Agency.woff2";
 import OptiTopicFontUrl from "./fonts/OPTITopic-Bold.woff2";
-
-let fullscreening = false;
-
-function toggleFullscreen() {
-  if (fullscreening) return;
-  fullscreening = true;
-  this.removeEventListener("click", toggleFullscreen);
-  const p = this.querySelector(".prompt");
-  if (p) {
-    p.classList.remove("hidden");
-  } else {
-    return;
-  }
-  const dd = this.querySelector(".daily");
-  if (dd) {
-    dd.classList.remove("hidden");
-    const ps = this.previousElementSibling;
-    if (ps && ps.querySelector(".daily")) ps.classList.remove("hidden");
-  }
-  const v = this.querySelector(".value");
-  if (v) v.classList.add("hidden");
-  setTimeout(() => {
-    this.classList.add("fullscreen");
-  }, 0);
-  const hf = this.querySelector(".horizontal-flip");
-  setTimeout(() => {
-    fullscreening = false;
-    if (hf) hf.addEventListener("click", horizontalFlip);
-    if (p) p.addEventListener("click", returnToBoard);
-  }, 1000);
-}
-
-function returnToBoard(e) {
-  this.classList.add("hidden");
-  let n = this.parentNode;
-  while (n != document.body && !n.classList.contains("fullscreen")) {
-    n = n.parentNode;
-  }
-  n.querySelectorAll(".blue-background *").forEach((n) =>
-    n.classList.add("hidden")
-  );
-  const dd = n.querySelector(".daily");
-  if (dd) {
-    const ps = n.previousElementSibling;
-    if (ps) {
-      const dd2 = ps.querySelector(".daily");
-      if (dd2) dd2.classList.add("hidden");
-    }
-    n.classList.add("hidden");
-  }
-  n.classList.remove("fullscreen");
-  e.stopPropagation();
-}
-
-function horizontalFlip() {
-  if (!this.parentNode.classList.contains("fullscreen"))
-    if (this.classList.contains("flipped")) return;
-  this.classList.add("flipped");
-}
-
-//document.querySelectorAll('.clues .little-screen').forEach(n => n.addEventListener('click', toggleFullscreen));
 
 const FONT_INFO = `
   @font-face {
@@ -95,6 +34,7 @@ const BASE_CLUE_VALUE = 200;
 const CATEGORY_COUNT = 6;
 const CLUES_PER_CAT = 5;
 const CATEGORIES = new Array(CATEGORY_COUNT).fill("POTENT POTABLES");
+const DAILY_DOUBLE = { column: 2, row: 3 };
 
 const clueInfoState = atomFamily({
   key: "clueInfo",
@@ -103,7 +43,8 @@ const clueInfoState = atomFamily({
     row,
     clue: "DRAUGHTS IS THIS KINGLY GAME",
     response: "What is checkers",
-    dailyDouble: column == 2 && row == 3,
+    dailyDouble: column == DAILY_DOUBLE.column && row == DAILY_DOUBLE.row,
+    flipped: false,
     used: false,
   }),
 });
@@ -125,26 +66,45 @@ export const Jeopardy = () => {
     `
   );
 
-  const selectedClue = useRecoilValue(selectedClueState);
+  // TODO(geophree): prevent clicking while fullscreening or flipping
+  const [selectedClue, setSelectedClue] = useRecoilState(selectedClueState);
   const clues = [];
   for (let j = 0; j < CATEGORY_COUNT; j++) {
     for (let i = 1; i <= CLUES_PER_CAT; i++) {
-      const { clue, dailyDouble, used } = useRecoilValue(clueInfoState([j, i]));
+      const [
+        { clue, dailyDouble, flipped, used },
+        setClueInfo,
+      ] = useRecoilState(clueInfoState([j, i]));
+      let selected = false;
+      let doClick = () => {};
       let before;
       let inner;
       if (!used) {
-        const selected = selectedClue?.column == j && selectedClue?.row == i;
+        if (selectedClue) {
+          selected = selectedClue.column == j && selectedClue.row == i;
+        } else {
+          doClick = () => setSelectedClue({ column: j, row: i });
+        }
+        if (selected) {
+          doClick = () => {
+            setClueInfo((info) => ({ ...info, used: true }));
+            setSelectedClue();
+          };
+        }
         const value = BASE_CLUE_VALUE * i;
         const showPrice = !selected;
         const showClue = selected;
         let clueSection = html`
-          <div class="prompt blue-background hidden">
+          <div class="prompt blue-background${showClue ? "" : " hidden"}">
             <p>${clue}</p>
           </div>
         `;
-        if (dailyDouble && selected) {
+        if (dailyDouble) {
+          if (selected && !flipped) {
+            doClick = () => {};
+          }
           const dd = html`
-            <div class="daily double">
+            <div class="daily double${selected ? "" : " hidden"}">
               <p>DAI<span class="daily-ly">LY</span><br />DOUBLE</p>
             </div>
           `;
@@ -153,9 +113,16 @@ export const Jeopardy = () => {
               <div class="limiter${used ? " blue-background" : ""}">${dd}</div>
             </div>
           `;
+          const doFlip = () =>
+            setClueInfo((info) => ({ ...info, flipped: true }));
           clueSection = html`
             <div class="vertical-flip">
-              <div class="horizontal-flip blue-background">
+              <div
+                class="horizontal-flip blue-background${flipped
+                  ? " flipped"
+                  : ""}"
+                onClick=${doFlip}
+              >
                 ${dd} ${clueSection}
               </div>
             </div>
@@ -163,7 +130,7 @@ export const Jeopardy = () => {
         }
         inner = html`
           ${clueSection}
-          <div class="value blue-background">
+          <div class="value blue-background${showPrice ? "" : " hidden"}">
             <p class=${value >= 1000 && "value-1000"}>
               <span class="dollar">$</span>${value}
             </p>
@@ -176,7 +143,10 @@ export const Jeopardy = () => {
           style=${{ "--column": j, "--row": i }}
         >
           ${before}
-          <div class="aspect-ratio little-screen">
+          <div
+            class="aspect-ratio little-screen${selected ? " fullscreen" : ""}"
+            onClick=${doClick}
+          >
             <div class="limiter${used ? " blue-background" : ""}">${inner}</div>
           </div>
         </div>
