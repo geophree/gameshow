db9216e5d5ed719f1faaa933a0df030dcf7eafe4
Jeff Hanke <geophree@gmail.com>
2020-12-29 16:27:55 -0800

    Add files I missed last time.

---
+++ dist/jeopardy/FinalReveal.js
@@ -0,0 +1,29 @@
+import { html } from "../web_modules/htm/react.js";
+import { useRecoilValue } from "../web_modules/recoil.js";
+
+import {
+  revealStageState,
+  selectedTeamState,
+  teamResponseState,
+  teamWagerState,
+} from "./state.js";
+
+export const FinalReveal = () => {
+  const name = useRecoilValue(selectedTeamState);
+  const nameStr = name ? `Team ${name}` : "";
+  const wager = useRecoilValue(teamWagerState(name));
+  const wagerStr = wager.toLocaleString("en-US");
+  const response = useRecoilValue(teamResponseState(name));
+  const revealStage = useRecoilValue(revealStageState(name));
+  const hideResponse = revealStage < 1 ? " hidden" : "";
+  const hideWager = revealStage < 2 ? " hidden" : "";
+  return html`
+    <div class="aspect-ratio blue-background final-reveal">
+      <p class="team-name">${nameStr}</p>
+      <p class="final-response${hideResponse}">${response}</p>
+      <p class="final-wager${hideWager}">
+        <span class="dollar">$</span>${wagerStr}
+      </p>
+    </div>
+  `;
+};
