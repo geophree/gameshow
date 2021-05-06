3cd2c5277fa44e1cd58e4c992c0dc5939757a041
Jeff Hanke <geophree@gmail.com>
2021-05-05 18:53:46 -0700

    Update to snowpack 3.3.7

--- snowpack.config.js
+++ snowpack.config.js
@@ -2,15 +2,11 @@ module.exports = {
   mount: {
     public: "/gameshow",
     src: "/gameshow/dist",
-    web_modules: "/gameshow/dist/web_modules",
   },
   plugins: ["@snowpack/plugin-dotenv"],
-  experiments: {
-    routes: [{ src: ".*", dest: "/gameshow/404.html", match: "routes" }],
-  },
+  routes: [{ src: ".*", dest: "/gameshow/404.html", match: "routes" }],
   buildOptions: {
-    metaDir: "/gameshow/dist/snowpack",
-    webModulesUrl: "/gameshow/dist/web_modules",
+    metaUrlPath: "/gameshow/dist/snowpack",
     minify: false,
     clean: true,
   },
